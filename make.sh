0a0e411d0cdbf21ec05eab834b3ea319e69f4ded
anon <anon@debian-BULLSEYE-live-builder-AMD64>
2023-07-19 20:37:25 -0700

    Add extra info

--- make.sh
+++ make.sh
@@ -6,6 +6,8 @@ set -e
 DIR=$(dirname "$0")
 
 bash $DIR/scripts/make-tex.sh
+git show -s --format=%as > $DIR/.tmp/date.tex
+git rev-parse --short HEAD > $DIR/.tmp/hash.tex
 
 cd $DIR/tex
 xelatex -halt-on-error -output-directory ./.. ./psalms.tex
